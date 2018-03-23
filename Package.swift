a68d364860014f12c345296e6c25519b741c71eb
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-03-23 10:47:52 -0400

    moved. initial

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+// Generated automatically by Perfect Assistant 2
+// Date: 2018-03-02 17:48:07 +0000
+import PackageDescription
+
+let package = Package(
+	name: "SAuthCodables",
+	products: [
+		.library(name: "SAuthCodables", targets: ["SAuthCodables"])
+	],
+	dependencies: [
+	],
+	targets: [
+		.target(name: "SAuthCodables", dependencies: [])
+	]
+)
